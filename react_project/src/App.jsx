665d955dcc749cd4cf613d56942efd1ce5655ada
seikaharuto21 <seikaharuto21@gmail.com>
2026-02-02 10:55:11 +0700

    update

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+function Header() {
+  return (
+    <div className="container">
+      <h1>Data Siswa</h1>
+      <p>Card React Sederhana</p>
+    </div>
+  );
+}
+
+function Biodata({ nama, nisn, kelas }) {
+  return (
+    <div className="card">
+      <div className="card-line"></div>
+
+      <h3>{nama}</h3>
+      <br />
+
+      <p>
+        <strong>NISN:</strong> {nisn}
+      </p>
+      <p>
+        <strong>Kelas:</strong> {kelas}
+      </p>
+
+      <div className="card-btn">
+        <button className="btn-edit">Edit</button>
+        <button className="btn-delete">Hapus</button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showModal, setShowModal] = useState(false);
+
+  const siswa = [
+    { nama: "Ezzar Poseidon", nisn: "1234", kelas: "pplg" },
+    { nama: "Ezzar Poseidon", nisn: "1234", kelas: "pplg" },
+    { nama: "Ezzar Poseidon", nisn: "1234", kelas: "pplg" },
+    { nama: "Ezzar Poseidon", nisn: "1234", kelas: "pplg" },
+    { nama: "Ezzar Poseidon", nisn: "1234", kelas: "pplg" },
+    { nama: "Ezzar Poseidon", nisn: "1234", kelas: "pplg" },
+    { nama: "Ezzar Poseidon", nisn: "1234", kelas: "pplg" },
+    { nama: "Ezzar Poseidon", nisn: "1234", kelas: "pplg" },
+    
+    
+    
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Header />
+
+      <div className="container">
+        <button onClick={() => setShowModal(true)}className="btn-add" >
+          + Tambah Siswa
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+        <div className="card-wrapper">
+          {siswa.map((item, index) => (
+            <Biodata
+              key={index}
+              nama={item.nama}
+              nisn={item.nisn}
+              kelas={item.kelas}
+            />
+          ))}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {showModal && (
+        <div className="modal">
+          <h3>Form Tambah</h3>
+          <input type="text" name="nama" id="nama"
+          placeholder="Nama Murid" />
+          <input type="text" name="nisn" id="nisn"
+          placeholder="NISN" />
+          <select name="kelas" id="kelas">
+            <option value="X PPLG 1">X PPLG 1</option>
+            <option value="X PPLG 1">X PPLG 2</option>
+            <option value="X PPLG 1">X PPLG 3</option>
+            <option value="X PPLG 1">XI PPLG 1</option>
+            <option value="X PPLG 1">XI PPLG 1</option>
+            <option value="X PPLG 1">XI PPLG 1</option>
+          </select>
+          <button>Simpan</button>
+          <button onClick={() => setShowModal(false)}>Tutup</button>
+        </div>
+      )}
+
+      <footer style={{ textAlign: "center", marginTop: "30px" }}>
+        © 2026
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
